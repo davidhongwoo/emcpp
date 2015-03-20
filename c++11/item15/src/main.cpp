8d4760f47cff0ec82ba7a338bd450811f9376186
hongwoo <hongwoo7@gmail.com>
2015-03-20 20:50:44 +0900

    item15...

--- c++11/item15/src/main.cpp
+++ c++11/item15/src/main.cpp
@@ -6,13 +6,13 @@ long long factorial(long long x) {
 	return x > 2 ? x + factorial(x-1) : 1;
 }
 #else
-constexpr long long factorial(long long x) {
+constexpr long long factorial(const long long x) {
 	return x > 0 ? x * factorial(x-1) : 1;
 }
 #endif
 
 int main() {
-	constexpr int i = 10000;
+	constexpr int i = 19;
 	cout << factorial(i) << endl;
 	return 0;
 }
